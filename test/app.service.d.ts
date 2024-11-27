47b49576036eb0cc0c60c2a8a56cc02f5f7b7012
Nahid <157556719+Nahidworld@users.noreply.github.com>
2024-11-27 21:00:57 +0600

    Add files via upload

---
+++ test/app.service.d.ts
@@ -0,0 +1,11 @@
+export declare class AppService {
+    getHello(): string;
+    getAb(): string;
+    private users;
+    add(data: any): {
+        message: string;
+        data: any;
+    };
+    getAll(): any[];
+    spec(name: any): any;
+}
