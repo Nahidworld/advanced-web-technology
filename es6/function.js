ef39af18af932203abcab7b158a077b22648e031
Nahid <157556719+Nahidworld@users.noreply.github.com>
2024-11-27 20:54:28 +0600

    Nest practice

---
+++ es6/function.js
@@ -0,0 +1,12 @@
+//without parameter
+function func1(){
+    console.log("Running Function 1 without parameter");
+}
+func1();
+
+//with parameter
+function func1(param="nahid"){
+    console.log(param);
+}
+func1();
+func1("nahiddd");
---
+++ es6/variable.js
@@ -0,0 +1,11 @@
+age = 20;
+console.log(age);
+var age = 18;
+console.log(age);       //age = 18
+var age = 28;
+console.log(age);       //age = 28
+
+if (age = 26){
+    let age=20;     //let is block element
+}
+console.log(age); 
